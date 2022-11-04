b25f5ebf85a3ca915d55402789df1b748f22505c
Prathibha <prathibha@Zipcoders-MacBook-Pro-8.local>
2022-11-03 21:59:00 -0400

    Implemented methods for AverageDouble list and test methods

--- src/main/java/AverageDoubleLists.java
+++ src/main/java/AverageDoubleLists.java
@@ -1,16 +1,24 @@
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class AverageDoubleLists {
 
     // write out these too
     public Double count(ArrayList<Double> aa) {
-        return null;
+        return Double.valueOf((aa.size()));
     }
     public Double sum(ArrayList<Double> aa) {
-        return null;
+        double sum=0.00;
+          DecimalFormat df=new DecimalFormat("0.00");
+        for(double number:aa) {
+            sum += number;
+        }
+        return Double.valueOf(df.format(sum));
     }
     public Double average(ArrayList<Double> aa) {
-        return null;
+        DecimalFormat df=new DecimalFormat("0.00");
+
+        return Double.valueOf(df.format(sum(aa)/aa.size()));
     }
 
 }
--- src/test/java/AverageDoubleListsTest.java
+++ src/test/java/AverageDoubleListsTest.java
@@ -1,21 +1,33 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AverageDoubleListsTest {
 
     @Test
     void count() {
-        assertEquals(null, "");
+        ArrayList<Double> aa=new ArrayList<Double>(Arrays.asList(5.60,7.80,9.20));
+        Double expected=3.00;
+        AverageDoubleLists obj=new AverageDoubleLists();
+        assertEquals(expected, obj.count(aa));
     }
 
     @Test
     void sum() {
-        assertEquals(null, "");
+        ArrayList<Double> aa=new ArrayList<Double>(Arrays.asList(5.60,7.80,9.20));
+        Double expected=22.60;
+        AverageDoubleLists obj=new AverageDoubleLists();
+        assertEquals(expected, obj.sum(aa));
     }
 
     @Test
     void average() {
-        assertEquals(null, "");
+        ArrayList<Double> aa=new ArrayList<Double>(Arrays.asList(5.6,7.8,9.2));
+        Double expected=7.53;
+        AverageDoubleLists obj=new AverageDoubleLists();
+        assertEquals(expected, obj.average(aa));
     }
 }
