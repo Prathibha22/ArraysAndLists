90088f5e8bff609e5a14d7be9b19a997e02ee80b
Prathibha <prathibha@Zipcoders-MacBook-Pro-8.local>
2022-11-04 18:18:02 -0400

    Created methods and testMethods for double types and passed all tests.

--- src/main/java/LoadAndStore.java
+++ src/main/java/LoadAndStore.java
@@ -15,39 +15,51 @@ public class LoadAndStore {
     }
 
     public ArrayList<Integer> loadIntegerArrayListFromFile(String filename) {
+//Refactored the code to use loadStringArrayListFromFile to all types.
+        ArrayList<Integer> alInt=new ArrayList<Integer>();
+        ArrayList<String> alString=loadStringArrayListFromFile(filename);
+        for(String str:alString)
+            alInt.add(Integer.parseInt(str));
 
-
-        // Creating an object of BufferedReader class
-        BufferedReader br = null;
-        try {
-            br = new BufferedReader(new FileReader(filename));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        String line ="";
-        ArrayList<Integer> result = new ArrayList<Integer>();
-
-        while (true) {
-            try {
-                if ((line = br.readLine()) == null) break; // break loop at end of file
-                if (line.startsWith("//")) continue; // ignore "//" comment lines
-                Integer number = Integer.parseInt(line);
-                result.add(number);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (NumberFormatException e) {
-                e.printStackTrace();
-            }
-        }
-
-        return result;
+        return alInt;
+//        // Creating an object of BufferedReader class
+//        BufferedReader br = null;
+//        try {
+//            br = new BufferedReader(new FileReader(filename));
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//        String line ="";
+//        ArrayList<Integer> result = new ArrayList<Integer>();
+//
+//        while (true) {
+//            try {
+//                if ((line = br.readLine()) == null) break; // break loop at end of file
+//                if (line.startsWith("//")) continue; // ignore "//" comment lines
+//                Integer number = Integer.parseInt(line);
+//                result.add(number);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            } catch (NumberFormatException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//
+//        return result;
     }
 
     // Build these out:
 
     // use testDoubleData2.txt to test
 
-    public double[] loaddoubleArrayFromFile(String filename) { return new double[]{}; }
+    public double[] loaddoubleArrayFromFile(String filename) {
+        ArrayList<Double> alDouble=loadDoubleArrayListFromFile(filename);
+        double[] arrDouble=new double[alDouble.size()];
+        for(int i=0;i<arrDouble.length;i++)
+            arrDouble[i]=alDouble.get(i);
+
+        return arrDouble;
+    }
 
     public ArrayList<Double> loadDoubleArrayListFromFile(String filename) {
 
@@ -61,7 +73,13 @@ public class LoadAndStore {
 
     // use testStringData3.txt to test
 
-    public String[] loadStringArrayFromFile(String filename) { return null; }
+    public String[] loadStringArrayFromFile(String filename) {
+        ArrayList<String> alStr=loadStringArrayListFromFile(filename);
+        String[] arrStr=new String[alStr.size()];
+        for(int i=0;i<arrStr.length;i++)
+            arrStr[i]=alStr.get(i);
+        return arrStr;
+    }
 
     public ArrayList<String> loadStringArrayListFromFile(String filename) {
         // Creating an object of BufferedReader class
--- src/test/java/LoadAndStoreTest.java
+++ src/test/java/LoadAndStoreTest.java
@@ -29,12 +29,18 @@ class LoadAndStoreTest {
 
     @Test
     void loadIntegerArrayListFromFile() {
-        assertEquals(null, "");
+        ArrayList<Integer> expected=new ArrayList<Integer>(Arrays.asList(4,5,6,11,13,8,9,7,14,18));
+        String fileName="testIntegerData1.txt";
+        LoadAndStore obj=new LoadAndStore();
+        assertEquals(expected, obj.loadIntegerArrayListFromFile(fileName));
     }
 
     @Test
     void loaddoubleArrayFromFile() {
-        assertEquals(null, "");
+        double[] expected={4.4,5.5,6.34,11.0001,13.004,8.7,9.97060,7.4532,14.5,18.00023132123};
+        String filename="testDoubleData2.txt";
+        LoadAndStore obj=new LoadAndStore();
+        assertArrayEquals(expected, obj.loaddoubleArrayFromFile(filename));
     }
 
     @Test
@@ -47,7 +53,11 @@ class LoadAndStoreTest {
 
     @Test
     void loadStringArrayFromFile() {
-        assertEquals(null, "");
+        String[] expected={"4.4","5.5","Foo","11.0001","Bar","Lost in Space","Lido Shuffle",
+                "How much data was destroyed?","I think therefore I compute","0","NaN","ZipCode Rocks!"};
+        String fileName="testStringData3.txt";
+        LoadAndStore obj=new LoadAndStore();
+        assertArrayEquals(expected, obj.loadStringArrayFromFile(fileName));
     }
 
     @Test
